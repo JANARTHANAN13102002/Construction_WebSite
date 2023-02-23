c5eb44a1bb247047479e2dcd64432946f6c27b40
JANARTHANAN B <95681443+JANARTHANAN13102002@users.noreply.github.com>
2023-02-23 09:20:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -9,4 +9,34 @@ let button = document.querySelector(".arrow"),
         button.children[2].classList.toggle("toggle2");
     })
 
+
+// Start of the Sliding images
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
+    
    
